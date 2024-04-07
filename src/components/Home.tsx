3f6a6e302685a4a066dbaf7f66c3e77f0cfa8a8c
Nazanin <earworm90@gmail.com>
2024-04-07 17:09:31 +0300

    Fix: change style and color of home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,18 +6,18 @@ export function loader() {
 }
 export default function Home() {
   return (
-    <div className="home-container container mt-5">
-      <div className="home-header row">
-        <div className="col-12 col-md-6 p-4">
+    <div className="home-section container mt-5 d-flex justify-content-center align-items-center">
+      <div className=" row main-row">
+        <div className="main-col col-12 col-md-6 p-4">
           <h1>Explore your next journey...</h1>
-          <p className="ms-5 mt-3">
+          <p className="ms-2 mt-3">
             Add your next favorite book or audio book to your library.
           </p>
           <Link to="/books" className="">
-            <button className="header-button">Find your book</button>
+            <button className="">Find your book</button>
           </Link>
         </div>
-        <div className="col-12 col-md-6 p-4 img-container "></div>
+        <div className="main-col col-12 col-md-6 p-4 img-container "></div>
       </div>
     </div>
   );
